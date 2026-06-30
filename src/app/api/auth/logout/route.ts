1890e214e11f4f7a68be75b198f2d339b84bf64f
thotsaporn Jaisue (TECH-TJ) <98930665+thotsaphon19@users.noreply.github.com>
2026-07-01 01:44:27 +0700

    Delete src directory

--- src/app/api/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-export const dynamic = 'force-dynamic'
-export async function GET() {
-  const res = NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/login`)
-  res.cookies.delete('session_token')
-  return res
-}
